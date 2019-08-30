f46813c9958076c4959cbfc1e481e4c722004bfd
Sam <samculver@gmail.com>
2019-08-30 15:10:35 -0700

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Header from "../components/header"
 const Home = () => (
   <Layout>
     <button onClick={() => navigate("/random/")}>Find a target</button>
-    <p>v 0.0.3</p>
+    <p className={styles.stats}>v 0.0.3</p>
   </Layout>
 )
 
